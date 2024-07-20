d4cb1555570f6c26b59225189b3ad521baaa1a89
Alireza <aliz.kazerani@gmail.com>
2024-07-20 02:15:31 -0400

    respond with body

--- app/server.rb
+++ app/server.rb
@@ -24,13 +24,39 @@ request_method = request_line.split(" ")[0].strip!
 request_target = request_line.split(" ")[1] # url path
 
 status = ""
+response_body = ""
+response_headers = {}
 if request_target == "/" 
     status = "HTTP/1.1 200 OK"
+elsif request_target.start_with? "/echo"
+    status = "HTTP/1.1 200 OK"
+    content = request_target["/echo".length + 1..]
+    puts content
+    response_body = content
+    response_headers["Content-Type"] = "text/plain"
 else
     status = "HTTP/1.1 404 Not Found"
 end 
 
-endOfStatusLine = "\r\n"
-response_headers = ""
-endOfHeaders = "\r\n"
-client_socket.write(status + endOfStatusLine + response_headers + endOfHeaders)
+
+if response_body.length > 0 
+  response_headers["Content-Length"] = response_body.length.to_s
+end
+puts "response headers length: " + response_headers.length.to_s
+
+
+end_of_status_line = "\r\n"
+response_headers_str = ""
+end_of_headers = "\r\n"
+
+end_of_header = "\r\n"
+response_headers.each do |key, value|
+    response_headers_str += key + ":" + value + end_of_header
+end
+
+puts "response_headers_str: " + response_headers_str
+
+client_socket.write(status + end_of_status_line + response_headers_str + end_of_headers + response_body)
+
+## Your /echo/{str} endpoint must return a 200 response, with the response body set to given string, and with a Content-Type and Content-Length header.
+

6bea00b953ae76d3553413b7df080ee7a88bcb9a
Alireza <aliz.kazerani@gmail.com>
2024-07-20 01:46:10 -0400

    extract url path

--- app/server.rb
+++ app/server.rb
@@ -8,8 +8,29 @@ print("Logs from your program will appear here!")
 server = TCPServer.new("localhost", 4221)
 client_socket, client_address = server.accept
 
+puts
+puts "client connected"
+
+raw_request = client_socket.gets
+
+puts 
+puts "raw request: " +  raw_request
+
+request_line = raw_request.split("\r\n")[0]
+
+puts "request line: " + request_line
+
+request_method = request_line.split(" ")[0].strip!
+request_target = request_line.split(" ")[1] # url path
+
+status = ""
+if request_target == "/" 
+    status = "HTTP/1.1 200 OK"
+else
+    status = "HTTP/1.1 404 Not Found"
+end 
 
 endOfStatusLine = "\r\n"
-header = ""
+response_headers = ""
 endOfHeaders = "\r\n"
-client_socket.write("HTTP/1.1 200 OK" + endOfStatusLine + endOfHeaders)
+client_socket.write(status + endOfStatusLine + response_headers + endOfHeaders)
